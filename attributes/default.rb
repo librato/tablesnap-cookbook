52b7ccd2d25d52d6b9746873b9604b7470b9a403
Mike Heffner <mikeh@fesnel.com>
2012-02-09 12:01:58 -0500

    Use tablesnap version 0.5.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:tablesnap][:repo] = "git://github.com/librato/tablesnap.git"
 
-default[:tablesnap][:version] = "0.4.2"
+default[:tablesnap][:version] = "0.5.0"
 
 default[:tablesnap][:logdir] = "/var/log/tablesnap"
